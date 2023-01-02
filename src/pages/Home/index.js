4dea72018e3c07b1fdc176474eaf90588ecda217
Joao Souza <joaoosouza07@gmail.com>
2023-01-01 23:59:00 -0300

    :sparkles: feat: Section skills

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,8 @@
 import './styles.css';
 import NavBar from "../../components/NavBar";
 
-import { RiGithubFill, RiLinkedinBoxFill, RiInstagramLine, RiDownloadCloud2Line} from 'react-icons/ri';
+import { SiHtml5, SiCss3, SiNodedotjs, SiSass, SiJavascript, SiTypescript, SiReact } from 'react-icons/si';
+import { RiGithubFill, RiLinkedinBoxFill, RiInstagramLine, RiDownloadCloud2Line } from 'react-icons/ri';
 
 import bgAction from '../../assets/program.svg';
 import profile from '../../assets/profile.jpg';
@@ -47,7 +48,7 @@ export default function Home() {
             {/***ABOUT***/}
 
             <section className="about">
-                <h3>Quem sou<span>?</span></h3>
+                <h3 className="title">Quem sou<span>?</span></h3>
                 <div className="container-about">
                     <div className="photo">
                         <img src={profile} alt="Foto de perfil" />
@@ -64,6 +65,64 @@ export default function Home() {
                     </div>
                 </div>
             </section>
+
+            {/***SKILLS***/}
+
+            <section className="skills">
+                <h3 className="title">Skills</h3>
+                <div className="container-skills">
+                    <div className="cards">
+                        <div className="card">
+                            <SiHtml5 size={32} color="#facc15" />
+                            <span>HTML</span>
+                            <p>É uma linguagem de marcação, onde marcamos os elementos para definir quais informações a página vai exibir.</p>
+                        </div>
+
+                        <div className="card">
+                            <SiCss3 size={32} color="#facc15" />
+                            <span>CSS</span>
+                            <p>É uma linguagem de folha de estilo composta por “camadas”, usado para estilizar as páginas HTML.</p>
+                        </div>
+
+                        <div className="card">
+                            <SiJavascript size={32} color="#facc15" />
+                            <span>JavaScript</span>
+                            <p>É uma linguagem de programação que permite implementar elementos dinâmicos em páginas web.</p>
+                        </div>
+
+                        <div className="card">
+                            <SiSass size={32} color="#facc15" />
+                            <span>Sass</span>
+                            <p>É um pré-processador CSS que adiciona alguns recursos que não estão disponíveis de forma nativa no CSS.</p>
+                        </div>
+
+                        <div className="card">
+                            <SiReact size={32} color="#facc15" />
+                            <span>React.js</span>
+                            <p>É uma biblioteca JavaScript com foco em criar interfaces de usuário de forma "componetizada".</p>
+                        </div>
+
+                        <div className="card">
+                            <RiGithubFill size={32} color="#facc15" />
+                            <span>Github</span>
+                            <p>É uma plataforma para hospedagem e controle de versão de código utilizando o sistema Git como base</p>
+                        </div>
+
+                        <div className="card">
+                            <SiTypescript size={32} color="#facc15" />
+                            <span>Typescript</span>
+                            <p>É um superset do JavaScript que incluir recursos que não estão presentes na linguagem, além de torná-la estática.</p>
+                        </div>
+
+                        <div className="card">
+                            <SiNodedotjs size={32} color="#facc15" />
+                            <span>Node.js</span>
+                            <p>É um ambiente de execução JavaScript, onde é possível criar aplicações sem depender do browser para isso.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
         </main>
     )
 }
